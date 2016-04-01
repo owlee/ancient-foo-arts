ff3156ac583279a146b2bbcdb5558559fc9e61a7
Oscar Lee <owlee@umass.edu>
2016-04-01 15:56:34 -0400

    Fix nil name...

--- app/models/Ign/video_api.rb
+++ app/models/Ign/video_api.rb
@@ -9,29 +9,29 @@ class Ign::VideoApi
 
       videos["data"].each do |video|
         Video.where(
-            name: video["metadata"]["headline"],
+            name: video["metadata"]["name"],
             thumbnail: video["thumbnail"],
             description: video["metadata"]["description"],
             title: video["metadata"]["title"],
             longTitle: video["metadata"]["longTitle"],
-            duration: video["metadata"]["duration"],#iteger
+            duration: video["metadata"]["duration"],
             url: video["metadata"]["url"],
             ageGate: video["metadata"]["ageGate"],
             classification: video["metadata"]["classification"],
             subClassification: video["metadata"]["subClassification"],
             networks: video["metadata"]["networks"],
-            noads: video["metadata"]["noads"], #boolean
-            prime: video["metadata"]["prime"], #boolean
-            subscription: video["metadata"]["subscription"], #boolean
-            downloadable: video["metadata"]["downloadable"], #boolean
+            noads: video["metadata"]["noads"],
+            prime: video["metadata"]["prime"],
+            subscription: video["metadata"]["subscription"],
+            downloadable: video["metadata"]["downloadable"],
             origin: video["metadata"]["origin"],
             genre: video["metadata"]["genre"],
             state: video["metadata"]["state"],
             slug: video["metadata"]["slug"],
             publishDate: video["metadata"]["publishDate"].to_datetime
         ).first_or_create
-
       end
+
      else
       #throw some error
     end
