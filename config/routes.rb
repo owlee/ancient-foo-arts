05f30746fe2b4061f972f9ee9a1a7f3675304abb
Oscar Lee <owlee@umass.edu>
2016-03-31 23:20:37 -0400

    rails server displays correct route, controller action, and test header

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'remove/posts'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'post#index', as: '/'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
