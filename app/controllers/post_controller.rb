618a650815073b93a9cf94560a0d9b8aa251bb3d
Oscar Lee <owlee@umass.edu>
2016-04-01 15:38:24 -0400

    Possible setup

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,12 @@
 class PostController < ApplicationController
   def index
+
+    binding.pry
     @articles = Ign::Article.get_articles
     @videos = Ign::Video.get_videos
+
+    @count = articles["count"] + videos["count"]
+    @posts << articles["data"] << videos["data"]
+
   end
 end
