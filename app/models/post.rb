ce3c77618e863a0ef2b9d657f1008624da37a11e
Oscar Lee <owlee@umass.edu>
2016-04-04 15:06:07 -0400

    Time Logic for how long ago was the post posted. NOT TESTED YET HENCE binding.pry

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug
   belongs_to :postable, polymorphic: true
+
+  def publish_age
+    binding.pry
+    age = Hash.new
+    age[:sec] = Time.now - self.publishDate
+    age[:hour] = age[:sec]/60
+    age[:day] = age[:sec]/3600
+    age
+  end
 end
