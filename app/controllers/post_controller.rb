5c7fbbb0190877ddd19b0f4c26596960856986f2
Oscar Lee <owlee@umass.edu>
2016-04-13 01:14:33 -0400

    Touch ups with erb an empty space and fattening up the button bar

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,21 +1,23 @@
 class PostController < ApplicationController
+  @@per_page = 10
+
   def index
   end
 
   def articles
-    @articles = Post.all.where(postable_type: 'Article').paginate(page: params[:page], per_page: 10).order('publishDate DESC')
+    @articles = Post.all.where(postable_type: 'Article').paginate(page: params[:page], per_page: @@per_page).order('publishDate DESC')
     @arr = @articles.map.with_index(1)
   end
 
   def appview
-    @posts = Post.all.paginate(page: params[:page], per_page: 10).order('publishDate DESC')
+    @posts = Post.all.paginate(page: params[:page], per_page: @@per_page).order('publishDate DESC')
   end
 
   def show
   end
 
   def videos
-    @videos = Post.all.where(postable_type: 'Video').paginate(page: params[:page], per_page: 10).order('publishDate DESC')
+    @videos = Post.all.where(postable_type: 'Video').paginate(page: params[:page], per_page: @@per_page).order('publishDate DESC')
     @ocunt = @videos.count
     @arr = @articles.map.with_index(1)
   end
